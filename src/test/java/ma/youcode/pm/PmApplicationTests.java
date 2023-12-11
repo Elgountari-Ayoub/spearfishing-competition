0be3d245a31aa39083c0bec19e3f54a1b13a0f34
Elgountari-Ayoub <elgountariayoub22@gmail.com>
2023-12-11 12:45:03 +0100

    creating the spring boot project

---
+++ src/test/java/ma/youcode/pm/PmApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.pm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
